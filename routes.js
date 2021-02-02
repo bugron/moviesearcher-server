f1826bb2586855ef3cf160ab5a8863188da74c44
Arsen Melikyan <bugron@mail.ru>
2021-02-02 20:35:12 +0400

    Remove log statements

--- routes.js
+++ routes.js
@@ -23,24 +23,18 @@ const authMiddleware = (req, res, next) => {
 }
 
 const omdbMiddleware = (req, res, next) => {
-  console.log(req.query);
-  // return res.json({ text: 'hello' });
-
   const { t, y = '', type = '', page = 1} = req.query;
-  // OMDB API request string: 
   const requestKey = `title:${t}-year:${y}-type:${type}-page:${page}`;
-  console.log('requestKey', requestKey);
+
   client.get(requestKey, (err, value) => {
     if (err) throw err;
 
-    console.log('db:value', value);
 
     if (value) {
       res.json(JSON.parse(value));
     } else {
       axios.get(`http://www.omdbapi.com/?apikey=3792a375&s=${t}&y=${y}&type=${type}&page=${page}`)
         .then(({ data }) => {
-          console.log('axios:data', data);
           let newData = {};
           if (page * 10 < +data.totalResults) {
             newData = Object.assign({}, data, { nextPage: Number(page) + 1 });
@@ -49,7 +43,6 @@ const omdbMiddleware = (req, res, next) => {
           }
           client.set(requestKey, JSON.stringify(newData), (err) => {
             if (err) throw err;
-            console.log('Respoinding with data');
             res.json(newData);
           })
         })
