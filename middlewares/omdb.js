886d243cfc8a50798a5d7e30119ad1f9d3ad3240
Arsen Melikyan <bugron@mail.ru>
2021-02-06 14:02:56 +0400

    Refactor omdb middleware

--- middlewares/omdb.js
+++ middlewares/omdb.js
@@ -7,33 +7,34 @@ client.on('error', function(error) {
 });
 
 const omdbMiddleware = (req, res, next) => {
+  const RESULTS_PER_PAGE = 10; // OMDb gives 10 items per page
   const { t, y = '', type = '', page = 1 } = req.query;
   // Create a key based on the request to cache search results
   const requestKey = `title:${t}-year:${y}-type:${type}-page:${page}`;
 
-  client.get(requestKey, (err, value) => {
+  client.get(requestKey, (err, chachedData) => {
     if (err) throw err;
 
     // If our search request is already cached
     // Then simply return cached results
-    if (value) {
-      res.json(JSON.parse(value));
+    if (chachedData) {
+      res.json(JSON.parse(chachedData));
     } else {
       // Otherwise make get data from omdb
       axios.get(`http://www.omdbapi.com/?apikey=3792a375&s=${t}&y=${y}&type=${type}&page=${page}`)
         .then(({ data }) => {
-          let newData = {};
+          let searchData = {};
           // Add some pagination info to the results
-          if (page * 10 < +data.totalResults) {
-            newData = Object.assign({}, data, { nextPage: Number(page) + 1 });
+          if (page * RESULTS_PER_PAGE < +data.totalResults) {
+            searchData = Object.assign({}, data, { nextPage: Number(page) + 1 });
           } else {
-            newData = data;
+            searchData = data;
           }
           // And finally cache the results
-          client.set(requestKey, JSON.stringify(newData), (err) => {
+          client.set(requestKey, JSON.stringify(searchData), (err) => {
             if (err) throw err;
             // And return the data to the client
-            res.json(newData);
+            res.json(searchData);
           })
         })
     }
